7423c797e4b510feff8dfb429e87cd6982b058bb
melissa.santos <melissa.santos@tenda.com>
2021-12-08 15:29:58 -0300

    conectado sql

--- contrato_venda.py
+++ contrato_venda.py
@@ -7,3 +7,9 @@ cnxn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};'
                       'PWD=b0u9t9p12;')
 
 cursor = cnxn.cursor() # gerenciador de comandos do banco
+cursor.execute('SELECT * FROM CONTATO;')
+
+for i in cursor.fetchall():
+    print('ID: ',i[0])
+    print('Telefone: ',i[1])
+    print('E-mail: ',i[2],'\n')
